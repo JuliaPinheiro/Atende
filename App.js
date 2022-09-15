f0c3dd102129b748acd9b9a3305b04f755d8773a
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2022-09-14 21:32:59 -0300

    Feat:✨init project

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import title from './src/components/title';
 
 const YourApp = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={{ flex: 1, justifyContent: "center" }}>
     <title/>
     </View>
   );
